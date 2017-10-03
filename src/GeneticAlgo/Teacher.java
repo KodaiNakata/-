2e89cc1e72c70182f1c6f98b1754db84a98b0065
KodaiNakata <pipo.nakaku49.nk@gmail.com>
2017-10-03 19:15:16 +0900

    先生クラスと時間割クラスを追加

--- src/GeneticAlgo/Teacher.java
+++ src/GeneticAlgo/Teacher.java
@@ -1,5 +1,52 @@
 package GeneticAlgo;
 
+// 先生のクラス
 public class Teacher {
+
+	String f_Name;// 氏名
+	int f_NumOfBasicSeminar1;// 基礎ゼミ1を持っているか
+	int f_NumOfBasicSeminar2;// 基礎ゼミ2を持っているか
+	int f_HasEngineeringDesignExperience;// エンジニアリングデザイン実験を持っているか
+	String f_ThemeOfElectricElectronicTraining;// 電気電子工学実習の何のテーマか
+	int f_NumOfElectricElectronicTraining;// 電気電子工学実習のコマ数
+	String f_ThemeOfElectricElectronicExperiment;// 電気電子工学実験の何のテーマか
+	int f_NumOfElectricElectronicExperiment;// 電気電子工学実験のコマ数
+	String f_ThemeOfEleInfoCommunicationExperiment;// エレ情実験の何のテーマか
+	String f_ThemeOfEnergyEnvironmentExperiment;// エネ環実験の何のテーマか
+	int f_NumOfGraduateStudySeminar;// 卒研ゼミのコマ数
+
+	/*
+	 * コンストラクタ
+	 * nullまたは0にする
+	 */
+	public Teacher(){
+		f_Name=null;
+		f_NumOfBasicSeminar1=0;
+		f_NumOfBasicSeminar2=0;
+		f_HasEngineeringDesignExperience=0;
+		f_ThemeOfElectricElectronicTraining=null;
+		f_NumOfElectricElectronicTraining=0;
+		f_ThemeOfElectricElectronicExperiment=null;
+		f_NumOfElectricElectronicExperiment=0;
+		f_ThemeOfEleInfoCommunicationExperiment=null;
+		f_ThemeOfEnergyEnvironmentExperiment=null;
+		f_NumOfGraduateStudySeminar=0;
+	}
+
+	/*
+	 * 氏名のゲッター
+	 * @return 氏名
+	 */
+	public String getName(){
+		return f_Name;
+	}
+
+	/*
+	 * 氏名のセッター
+	 * @param name 氏名
+	 */
+	public void setName(String name){
+		f_Name=name;
+	}
 
 }
--- src/GeneticAlgo/TimeTable.java
+++ src/GeneticAlgo/TimeTable.java
@@ -1,5 +1,113 @@
 package GeneticAlgo;
 
+// 時間割のクラス
 public class TimeTable {
 
+	private String f_DayOfWeek;// 曜日
+	private int f_Grade;// 学年
+	private String f_Course;// コース
+	private String f_Subject;// 科目名
+	private String f_Teacher;// 担当の先生
+
+	/*
+	 * コンストラクタ
+	 * 0またはnullにする
+	 */
+	public TimeTable(){
+		f_DayOfWeek=null;
+		f_Grade=0;
+		f_Course=null;
+		f_Subject=null;
+		f_Teacher=null;
+	}
+
+	/*
+	 * 曜日のゲッター
+	 *
+	 * @return 曜日
+	 */
+	public String getDayOfWeek() {
+		return f_DayOfWeek;
+	}
+
+	/*
+	 * 学年のゲッター
+	 *
+	 * @return 学年
+	 */
+	public int getGrade() {
+		return f_Grade;
+	}
+
+	/*
+	 * コースのゲッター
+	 *
+	 * @return コース
+	 */
+	public String getCourse() {
+		return f_Course;
+	}
+
+	/*
+	 * 科目名のゲッター
+	 *
+	 * @return 科目名
+	 */
+	public String getSubject() {
+		return f_Subject;
+	}
+
+	/*
+	 * 先生のゲッター
+	 *
+	 * @return 先生
+	 */
+	public String getTeacher() {
+		return f_Teacher;
+	}
+
+	/*
+	 * 曜日のセッター
+	 *
+	 * @param dayOfWeek 曜日
+	 */
+	public void setDayOfWeek(String dayOfWeek) {
+		f_DayOfWeek = dayOfWeek;
+	}
+
+	/*
+	 * 学年のセッター
+	 *
+	 * @param grade 学年
+	 */
+	public void setGrade(int grade) {
+		f_Grade = grade;
+	}
+
+	/*
+	 * コースのセッター
+	 *
+	 * @param course コース
+	 */
+	public void setCourse(String course) {
+		f_Course = course;
+	}
+
+	/*
+	 * 科目名のセッター
+	 *
+	 * @param subject 科目名
+	 */
+	public void setSubject(String subject) {
+		f_Subject = subject;
+	}
+
+	/*
+	 * 先生のセッター
+	 *
+	 * @param teacher 先生
+	 */
+	public void setTeacher(String teacher) {
+		f_Teacher = teacher;
+	}
 }
