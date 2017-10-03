480e74f91154b90f89358c9be948fedd0c3321ff
Nakata <pipo.nakaku49.nk@gmail.com>
2017-10-03 14:52:07 +0900

    Mainクラスのコメント削除

--- src/GeneticAlgo/Main.java
+++ src/GeneticAlgo/Main.java
@@ -3,7 +3,6 @@ package GeneticAlgo;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 
 		Decide_faculty Dec_fac = new Decide_faculty  ();
 		Decide_room Dec_room = new Decide_room  ();
