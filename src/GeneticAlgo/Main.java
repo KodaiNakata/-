508eb3b188617494d4828a18240a74037f6659e2
KodaiNakata <pipo.nakaku49.nk@gmail.com>
2017-11-08 20:28:06 +0900

    候補の時間割を複数作成(初期集団のみ)

--- src/GeneticAlgo/Main.java
+++ src/GeneticAlgo/Main.java
@@ -1,5 +1,6 @@
 package GeneticAlgo;
 
+
 /*
  * メインクラス
  * @author Nakata
@@ -8,11 +9,18 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//		Decide_faculty dec_faculty=new Decide_faculty();
-//		dec_faculty.exe();
-		
-		Decide_dayAndPeriod dec_dayPeriod = new Decide_dayAndPeriod  ();
+		// Decide_faculty dec_faculty=new Decide_faculty();
+		// dec_faculty.exe();
+
+		Decide_dayAndPeriod dec_dayPeriod = new Decide_dayAndPeriod();
 		dec_dayPeriod.exe();
+
+//		ArrayList<String>[] list = new ArrayList[1];
+//		for (int i = 0; i < list.length; i++)
+//			list[i] = new ArrayList<String>();
+//		list[0].add("ドーナツ");
+//
+//		System.out.println(list[0].get(0));
 	}
 
 }
