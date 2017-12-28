c360748263efa3cfa019c26408a2a656895ab49a
KodaiNakata <pipo.nakaku49.nk@gmail.com>
2017-12-28 16:50:59 +0900

    再履修クラスもカウントさせる

--- src/GeneticAlgo/Evaluation.java
+++ src/GeneticAlgo/Evaluation.java
@@ -1,16 +1,16 @@
 package GeneticAlgo;
 
-import java.util.ArrayList;
 
 /*
  * 評価値のクラス
  *
  * @author Nakata
  */
-public class Evaluation {
+public class Evaluation implements iDayPeriod{
 
 	private int f_Generation;// 世代
-	private ArrayList<Integer> f_EvaluationValues=new ArrayList<Integer>();
+//	private ArrayList<Integer> f_EvaluationValues=new ArrayList<Integer>();
+	private int[] f_EvaluationValues=new int[CANDIDATE_NUM];
 	private int f_SumEvaluationValue;// 評価値
 
 	/*
@@ -19,16 +19,22 @@ public class Evaluation {
 	public Evaluation() {
 		f_Generation = -1;
 		f_SumEvaluationValue = 0;
+		
+		for(int candidate=0;candidate<CANDIDATE_NUM;candidate++){
+			f_EvaluationValues[candidate]=0;
+		}
+		
+//		f_EvaluationValues.clear();
 	}
 
-	/*
-	 * 各評価値の動的配列の追加
-	 *
-	 * @param value 評価値
-	 */
-	public void addEvaluationValues(int value){
-		f_EvaluationValues.add(value);
-	}
+//	/*
+//	 * 各評価値の動的配列の追加
+//	 *
+//	 * @param value 評価値
+//	 */
+//	public void addEvaluationValues(int value){
+//		f_EvaluationValues.add(value);
+//	}
 
 	// ----------------------------//
 	// ---------ゲッター-----------//
@@ -42,17 +48,25 @@ public class Evaluation {
 		return f_Generation;
 	}
 
-	/*
-	 * 各評価値のゲッター
-	 *
-	 * @param number 要素番号
-	 *
-	 * @return 指定された要素の評価値
-	 */
-	public int getEvaluationValues(int number){
-		return f_EvaluationValues.get(number);
-	}
+//	/*
+//	 * 各評価値のゲッター
+//	 *
+//	 * @param number 要素番号
+//	 *
+//	 * @return 指定された要素の評価値
+//	 */
+//	public int getEvaluationValues(int number){
+//		return f_EvaluationValues.get(number);
+//	}
 
+	public int[] getArrayOfEvaluationValues(){
+		return f_EvaluationValues;
+	}
+	
+	public int getEvaluationValues(int candidate){
+		return f_EvaluationValues[candidate];
+	}
+	
 	/*
 	 * 評価値のゲッター
 	 *
@@ -74,6 +88,10 @@ public class Evaluation {
 		f_Generation = generation;
 	}
 
+	public void setEvaluationValues(int candidate,int[] evaluation,int value){
+		evaluation[candidate]=value;
+	}
+	
 	/*
 	 * 評価値のセッター
 	 *
--- src/GeneticAlgo/iDayPeriod.java
+++ src/GeneticAlgo/iDayPeriod.java
@@ -4,8 +4,8 @@ package GeneticAlgo;
  * 曜日と限目のインタフェース
  */
 public interface iDayPeriod {
-	public final int MAX_DAY = TimeTable.changeDayToValue("金");// 月曜から何曜日までか
-	public final int MAX_PERIOD = 5;// 5限目
+	public final int MAX_DAY = TimeTable.changeDayToValue("金");// 何曜日までか
+	public final int MAX_PERIOD = 5;// 何限目までか
 	public final int MAX_SEMESTER=ClassOfGrade.changeSemesterToValue("後期");
 	public final int CANDIDATE_NUM = 3;// 候補の数
 }
